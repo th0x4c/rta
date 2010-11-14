1afe0bedcd6963c13752f5e8adca20fcb0a1f5f3
hashi <hashi@b51d36c4-7d92-49ba-b7cf-209faa32988c>
2010-11-14 11:27:11 +0000

    Add examples directory.

---
+++ examples/ex1/ex1.rb
@@ -0,0 +1,52 @@
+class ExampleSession < RTA::Session
+  def setup
+    # 接続
+    begin
+      # java.lang.Class.forName("oracle.jdbc.driver.OracleDriver")
+      url = "jdbc:oracle:thin:@192.168.1.5:1521:XE"
+      user = "scott"
+      passwd = "tiger"
+      @con = DriverManager.getConnection(url, user, passwd)
+      @con.setAutoCommit(false)
+    rescue SQLException => e
+      e.cause.printStackTrace
+    rescue ClassNotFoundException => e
+      e.cause.printStackTrace
+    end
+
+    # トランザクション
+    @tx1 = RTA::Transaction.new("tx1") do
+      stmt = @con.prepareStatement("select ename, comm from emp where empno = 7900")
+      rset = stmt.executeQuery
+      while rset.next
+        log.info("sid: #{@session_id} " + rset.getString(1) + " " + rset.getInt(2).to_s)
+      end
+      rset.close
+      stmt.close
+    end
+    @tx1.after { sleep 1 }
+
+    @tx2 = RTA::Transaction.new("tx2") do
+      stmt = @con.createStatement
+      stmt.executeUpdate("update emp set comm = #{@session_id} where empno = 7900")
+      @con.commit
+      stmt.close
+    end
+    @tx2.after { sleep 0.5 }
+
+    # ログ
+    # self.log = RTA::Log.new("./test_#{@session_id}.log")
+    # self.log.level = RTA::Log::DEBUG
+  end
+
+  def transaction
+    # tx1 と tx2 を交互に実行
+    count = @tx1.count + @tx2.count
+    tx = count % 2 == 0 ? @tx1 : @tx2
+    return tx
+  end
+
+  def teardown
+    @con.close
+  end
+end
